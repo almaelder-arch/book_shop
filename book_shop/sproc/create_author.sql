647366b9ab37301f9b20edfd8313b0ef554e6563
Alma Elder <almaelder83@gmail.com>
2025-09-29 17:02:25 +0100

    v0.0.1 -- Adding Flyway Schema History Table and a sproc

---
+++ book_shop/sproc/create_author.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[create_author]
+	@first_name NVARCHAR(30) ,
+	@last_name NVARCHAR(30)
+AS
+BEGIN
+
+	DECLARE @id INT
+	
+	INSERT INTO dbo.author(first_name,last_name)
+	VALUES(@first_name,@last_name)
+
+	SET @id = SCOPE_IDENTITY()
+
+	SELECT @id
+
+END
+GO
+
---
+++ book_shop/tables/flyway_schema_history.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[flyway_schema_history](
+	[installed_rank] [int] NOT NULL,
+	[version] [nvarchar](50) NULL,
+	[description] [nvarchar](200) NULL,
+	[type] [nvarchar](20) NOT NULL,
+	[script] [nvarchar](1000) NOT NULL,
+	[checksum] [int] NULL,
+	[installed_by] [nvarchar](100) NOT NULL,
+	[installed_on] [datetime] NOT NULL,
+	[execution_time] [int] NOT NULL,
+	[success] [bit] NOT NULL,
+ CONSTRAINT [flyway_schema_history_pk] PRIMARY KEY CLUSTERED 
+(
+	[installed_rank] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[flyway_schema_history] ADD  DEFAULT (getdate()) FOR [installed_on]
+GO
