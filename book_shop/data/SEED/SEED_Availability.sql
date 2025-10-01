5685e798563ab4755c236d40db5798be62853663
Alma Elder <almaelder83@gmail.com>
2025-10-01 13:03:13 +0100

    Seed/Test Data & New Search SP

---
+++ book_shop/data/SEED/SEED_Availability.sql
@@ -0,0 +1,11 @@
+﻿USE book_shop
+GO
+
+/****
+SCRIPT TO SEED THE AVAILABILITY TABLE
+****/
+
+INSERT INTO dbo.[availability](availability_code,availability_desc)
+VALUES(0,'Out Of Stock'),
+(1,'Available'),
+(2,'Pre-Order')
---
+++ book_shop/sproc/getBooksByCatCode.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[getBooksByCatCode]
+	@code NVARCHAR(6) 
+AS
+BEGIN
+	SELECT 
+		b.[title],
+		b.[ISBN],
+		b.[Description],
+		b.[release_date],
+		a.[first_name],
+		a.[last_name],
+		av.[availability_desc]
+	FROM dbo.books b WITH(NOLOCK)
+	JOIN dbo.category c WITH(NOLOCK) ON c.category_id = b.category_id
+	JOIN dbo.author a WITH(NOLOCK) ON a.author_id = b.author_id
+	JOIN dbo.[availability] av WITH(NOLOCK) ON av.availability_id = b.availability_id
+	where c.category_code = @code
+END
